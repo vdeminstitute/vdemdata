e1064858889a4efdf4724a874f2bcd11370bf88f
Seraphine F. Maerz <seraphine.maerz@v-dem.net>
2019-11-28 17:33:14 +0100

    upload

---
+++ R/var_info.R
@@ -0,0 +1,26 @@
+#' Shows basic information on a specific variable as given in the codebook
+#'
+#' Provides information on full name as specified in the codebook, variable type,
+#' data collection process (questions, clarifications, responses),
+#' scale, aggregation, years and sources (plus notes)
+#' of a variable in the V-Dem data set
+#'
+#' @param var_tag A variable's name as used in the V-Dem data set,
+#' e.g. "v2x_polyarchy" for the electoral democracy index.
+#' Note: use quotation marks vor "var_tag".
+#'
+#' @return A list of information on the desired variable.
+#'
+#' @import tidyverse
+#' @export
+#'
+#' @examples
+#' # Get information on the variable "v2x_polyarchy" (the electoral democracy index)
+#' var_info("v2x_polyarchy")
+var_info <- function(var_tag){
+  codebook %>%
+    filter(tag == var_tag) %>%
+    as.list()
+}
+
+
