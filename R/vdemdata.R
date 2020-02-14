15ad01fa36d4ac0cdea70a1c78fca9f87862c4ea
V-Dem (Varieties of Democracy) Institute <seraphine.maerz@v-dem.net>
2020-02-14 13:36:39 +0100

    Update vdemdata.R

--- R/vdemdata.R
+++ R/vdemdata.R
@@ -3,7 +3,8 @@
 #' A package to load, expore and work with the most recent V-Dem (Varieties of Democracy) dataset.
 #' 
 #' @docType package
-#' @author Seraphine Maerz <seraphine.maerz@gu.se>
+#' @author Seraphine F. Maerz <seraphine.maerz@v-dem.net>, Amanda B. Edgell <amanda.edgell@v-dem.net>
+#' @references \url\{https://github.com/vdeminstitute/vdemdata} and \url{https://www.v-dem.net/en/}
 #' @importFrom Rcpp evalCpp
 #' @useDynLib vdemdata
 #' @name vdemdata
